2c8660bd961847129fae60408e982c5b55835474
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-01-31 11:10:21 +0900

    Title: 두 번의 연산, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/두 번의 연산/two-operations.py
+++ 240131/두 번의 연산/two-operations.py
@@ -1,14 +1,23 @@
 a = int(input())
 
-if a % 2 != 0:
-    a += 3
-    if a % 3 == 0:
-        print(int(a/3))
-    else:
-        print(a)
+# if a % 2 != 0:
+#     a += 3
+#     if a % 3 == 0:
+#         print(int(a/3))
+#     else:
+#         print(a)
 
-else:
-    if a % 3 == 0:
-        print(int(a/3))
-    else:
-        print(a)
+# else:
+#     if a % 3 == 0:
+#         print(int(a/3))
+#     else:
+#         print(a)
+
+# 해설 - 아 조건에 맞을 경우에만 연산을 하면 되니까 굳이 else로 아닐 경우를 적어줄 필요가 없음
+if a % 2 == 1:
+    a += 3
+    
+if a % 3 == 0:
+    a //= 3
+
+print(a)
