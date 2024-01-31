9527510891f1eeabacfdaa4663696102e6bce719
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-01-31 11:08:00 +0900

    Title: 두 번의 연산, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240131/두 번의 연산/two-operations.py
@@ -0,0 +1,14 @@
+a = int(input())
+
+if a % 2 != 0:
+    a += 3
+    if a % 3 == 0:
+        print(int(a/3))
+    else:
+        print(a)
+
+else:
+    if a % 3 == 0:
+        print(int(a/3))
+    else:
+        print(a)
