9881a12bb68038afba5d37b35f59e73683db3b76
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-01-31 11:51:24 +0900

    Title: 1부터 n까지 출력, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/1부터 n까지 출력/print-from-1-to-n.py
+++ 240131/1부터 n까지 출력/print-from-1-to-n.py
@@ -1,3 +1,4 @@
+# 내 답안
 n = int(input())
 
 list_ = []
@@ -5,4 +6,9 @@ list_ = []
 for i in range(1,n+1):
     list_.append(str(i))
 
-print((' ').join(list_))
+# print((' ').join(list_))
+
+
+# 해설 답안
+for i in range(1,n+1):
+    print(i, end=' ')
