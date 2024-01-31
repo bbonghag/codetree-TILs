94dc125dc1cb9676153c166a7bac31281fae3331
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-01-31 11:46:01 +0900

    Title: 중앙값 구하기, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/중앙값 구하기/find-the-median.py
+++ 240131/중앙값 구하기/find-the-median.py
@@ -1,8 +1,33 @@
-n = list(map(int, input().split()))
+# 내 답안
+# n = list(map(int, input().split()))
 
-max_n = max(n)
-min_n = min(n)
+# max_n = max(n)
+# min_n = min(n)
 
-n.remove(max_n)
-n.remove(min_n)
-print(n[0])
+# n.remove(max_n)
+# n.remove(min_n)
+# print(n[0])
+
+# 해설
+a,b,c = map(int, input().split())
+
+
+# 출력
+if a > b:
+    if c > a:
+        # a > b, c > a 일때 (c > a > b)
+        print(a)
+    # a > b, a > c 일때 (a가 가장 크고, b와 c중 더 큰 수가 중앙값)
+    elif b > c:
+        print(b)
+    else:
+        print(c)
+else:
+    if c > b:
+        # b > a, c > b 일때 (c > b > a)
+        print(b)
+    # b > a, b > c 일때 (b가 가장 크고, a와 c중 더 큰 수가 중앙값)
+    elif a > c:
+        print(a)
+    else:
+        print(c)
