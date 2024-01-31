0e9fa549626771b24c3cc387f1f1c24a839a6685
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-01-31 11:30:20 +0900

    Title: 두 사람, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240131/두 사람/two-person.py
+++ 240131/두 사람/two-person.py
@@ -1,8 +1,8 @@
 p1 = input().split()
 p2 = input().split()
 
-print(p1)
-print(p2)
+# print(p1)
+# print(p2)
 
 if ((int(p1[0]) >= 19) & (p1[1]=='M')) or ((int(p2[0]) >= 19) & (p2[1]=='M')):
     print(1)
