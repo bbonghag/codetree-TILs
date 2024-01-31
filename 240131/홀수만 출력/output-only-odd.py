94b0dcb3d3afb180af8c6a58301ec4a835a39575
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-01-31 11:57:41 +0900

    Title: 홀수만 출력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240131/홀수만 출력/output-only-odd.py
+++ 240131/홀수만 출력/output-only-odd.py
@@ -1,6 +1,12 @@
-a,b = map(int, input().split())
+# 내 답안 - a,b 는 홀수라고 조건을 줬으므로 굳이 조건문을 쓸 필요가 없었음
+# a,b = map(int, input().split())
 
-for i in range(a,b+1):
-    # print(i, end=' ')
-    if i % 2 != 0:
-        print(i, end=' ')
+# for i in range(a,b+1):
+#     # print(i, end=' ')
+#     if i % 2 != 0:
+#         print(i, end=' ')
+
+
+# 해설
+for i in range(a,b+1,2):
+    print(i, end=' ')
