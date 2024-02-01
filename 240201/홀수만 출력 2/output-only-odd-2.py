773ae166ae254c533b8fdd5f8f395276d35937c0
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-02-01 12:23:02 +0900

    Title: 홀수만 출력 2, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240201/홀수만 출력 2/output-only-odd-2.py
+++ 240201/홀수만 출력 2/output-only-odd-2.py
@@ -1,10 +1,11 @@
 b,a = map(int, input().split())
 
 
-for i in range(b,a-1, -1):
-    if i % 2 != 0:
-        print(i, end=' ')
-
+# b,a는 홀수라고 정의되어 있으므로 조건문을 줄 필요가 없음
+for i in range(b,a-1, -2):
+    # if i % 2 != 0:
+    #     print(i, end=' ')
+    print(i, end=' ')
 
 # 생각난 예시
 # for i in range(7,1,-1):
