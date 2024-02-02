4b73207622af2efc5473b0bc12141cca7fcc0483
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-02-02 12:11:14 +0900

    Title: 1까지 나누기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240202/1까지 나누기/divide-by-1.py
+++ 240202/1까지 나누기/divide-by-1.py
@@ -14,3 +14,21 @@ while True:
         n = d
 
 print(count)
+
+# 해설 - for문도 가능
+# for i in range(1, 101):
+#     if n <= 1:
+#         print(cnt)
+#         break
+#     n //= i
+#     cnt += 1
+
+
+# 해설 - 아 while에 조건을 걸면 좀 더 코드가 간단해지겠구나! 
+# i = 1
+# while n > 1:
+#    n //= i
+#    i += 1
+#    cnt += 1
+
+# print(cnt)
