286408c000718efa0c86f6aed8879df5533d4877
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-02-03 23:16:43 +0900

    Title: 직각 삼각형 출력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240203/직각 삼각형 출력/Right-triangle-and-output.py
+++ 240203/직각 삼각형 출력/Right-triangle-and-output.py
@@ -4,3 +4,13 @@ k = 1
 for _ in range(1,n+1):
     print(k*'*')
     k += 2
+
+# 해설 - n번만큼 1차 for문을 돌리고 2차 for문에서 각 순서마다
+# 변수 선언 및 입력
+n = int(input())
+
+# 길이가 n인 직각삼각형을 출력합니다.
+for i in range(n):
+	for _ in range(2 * i + 1):
+		print("*", end="")
+	print()
