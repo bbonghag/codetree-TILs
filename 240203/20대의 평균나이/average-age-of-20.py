6360d5f8bb7d1a53b559eb032f8bf8ebac6b10c0
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-02-03 22:35:58 +0900

    Title: 20대의 평균나이, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240203/20대의 평균나이/average-age-of-20.py
+++ 240203/20대의 평균나이/average-age-of-20.py
@@ -9,4 +9,25 @@ while True:
     else:
         result = round((age_sum/count), 2)
         print(f"{result:.2f}")    
-        break # 특정 조건에서 while문을 끊어주지 않으면 EOFError뜸.
+        break # 특정 조건에서 while문을 끊어주지 않으면 EOFError뜸. 
+
+
+# 해설
+# # 변수 선언
+# sum_val = 0
+# cnt = 0
+
+# # 언제 끝날지 모르므로
+# # 계속 반복합니다.
+# while True:
+#     # 변수를 선언하고 입력을 받습니다.
+#     n = int(input())
+
+#     # 입력받은 값이 20대가 아니면 종료합니다.
+#     if n >= 30 or n <= 19:
+#         print(f"{sum_val/cnt:.2f}")
+#         break
+    
+#     # 20대가 맞는 경우에는, 계속 값을 계산해줍니다.
+#     sum_val += n
+#     cnt += 1
