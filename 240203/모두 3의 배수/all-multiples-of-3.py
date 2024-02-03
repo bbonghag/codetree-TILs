1c85fcb8f89ebede57cf0f9dfde8e7c5dc1f7d2c
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-02-03 22:54:59 +0900

    Title: 모두 3의 배수, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240203/모두 3의 배수/all-multiples-of-3.py
+++ 240203/모두 3의 배수/all-multiples-of-3.py
@@ -8,3 +8,19 @@ if cnt == 5:
     print(1)
 else:
     print(0)
+
+# 해설 - bool타입을 주로 이용함.더하는 과정을 생략할 수 있어서 이것도 괜찮아보임
+# # 변수 선언 및 입력
+# satisfied = True
+	
+# for _ in range(5):
+#     # 모든 수가 3의 배수인지 확인합니다.
+#     a = int(input())
+#     if a % 3 != 0:
+#         satisfied = False
+
+# #출력
+# if satisfied == True:
+#     print("1")
+# else:
+#     print("0")
