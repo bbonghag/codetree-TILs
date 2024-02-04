de625f4a28afd5c6e417f43053a570118e79ce4e
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-02-04 20:02:00 +0900

    Title: 건너뛰고 별표 출력하기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240204/건너뛰고 별표 출력하기/skip-and-print-a-star.py
+++ 240204/건너뛰고 별표 출력하기/skip-and-print-a-star.py
@@ -1,11 +1,27 @@
 n = int(input())
-cnt = 0
 
-for i in range(1, n + n):
-    print(i*'*','\n')
+# 첫번째 내 풀이
+# cnt = 0
+
+# for i in range(1, n + n):
+#     print(i*'*','\n')
+#     cnt += 1
+#     if cnt == n:
+#         break
+
+# for j in range(n-1, 0, -1):
+#     print(j*'*','\n')
+
+
+####################################
+# 두번째 내 풀이
+
+cnt = 1
+
+for _ in range(n):
+    print(cnt*'*','\n')
     cnt += 1
-    if cnt == n:
-        break
-
-for j in range(n-1, 0, -1):
-    print(j*'*','\n')
+    if cnt == n+1:
+        for i in range(n-1,0,-1):
+            print(i*'*','\n')
+            # print(i)
