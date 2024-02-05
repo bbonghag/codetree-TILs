20c72bb340cb00c35abb1fa5d461fac2cfb60b66
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-02-05 18:09:43 +0900

    Title: 최대공약수 구하기, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240205/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240205/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,3 +1,4 @@
+# 내 풀이 - 
 n,m = map(int, input().split())
 
 def max_nxm(n,m):
@@ -13,3 +14,21 @@ def max_nxm(n,m):
         print(max(d))
 
 max_nxm(n,m)
+
+# 해설  
+# 이걸보니 최대 공약수가 될 수 있는건 둘 중 작은 수이므로 굳이 100까지 for문을 돌릴 필요가 없음. 
+# 둘 중 작은 수를 기준으로 for문을 돌려도됨
+# 그리고 for문을 돌수록 더 큰 값이 나오면서 결국에는 가장 큰값으로 m,n 둘중 하나의 값이 나오기때문에
+# 따로 리스트에 추가해서 최대값을 구할 필요가 없음
+
+# n과 m의 최대공약수를 반환합니다.
+# def find_gcd(n, m):
+#     gcd = 0
+#     for i in range(1, min(n, m) + 1):
+#         if n % i == 0 and m % i == 0:
+#             gcd = i
+
+#     print(gcd)
+
+
+# find_gcd(n, m)
