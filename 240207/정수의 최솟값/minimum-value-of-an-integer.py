0446d5c17adbe3243a2f7e6acb2bd2f31526ec82
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-02-07 14:48:52 +0900

    Title: 정수의 최솟값, Time: 157ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/정수의 최솟값/minimum-value-of-an-integer.py
+++ 240207/정수의 최솟값/minimum-value-of-an-integer.py
@@ -1,7 +1,14 @@
 a,b,c = map(int, input().split())
 
 def n_min(a,b,c):
-    return min([a,b,c])
+    # return min([a,b,c])
+    tem_m = a
+    if a > b:
+        tem_m = b
+    if tem_m > c:
+        tem_m = c
+    
+    return tem_m
 
 m = n_min(a,b,c)
 print(m)
