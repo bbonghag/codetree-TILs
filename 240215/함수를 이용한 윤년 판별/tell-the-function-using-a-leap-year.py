340f3796cbedd095805a8a520645cf1c1a3cd1c9
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-02-15 11:56:20 +0900

    Title: 함수를 이용한 윤년 판별, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240215/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,13 +1,16 @@
 y = int(input())
 
 def year(y):
-    if y % 4 == 0:
-        return True
-    if (y % 100 == 0) & (y % 4 == 0):
-        return False
     if (y % 100 == 0) & (y % 4 == 0) & (y % 400 == 0):
         return True
-    return True
+
+    if (y % 100 == 0) & (y % 4 == 0):
+        return False   
+
+    if y % 4 == 0:
+        return True
+
+
 
 result = year(y)
 
