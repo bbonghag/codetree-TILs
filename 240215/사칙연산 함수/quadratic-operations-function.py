6c53c34dceb2467a3312c62a9020845e86c67fd6
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-02-15 12:26:27 +0900

    Title: 사칙연산 함수, Time: 173ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/사칙연산 함수/quadratic-operations-function.py
+++ 240215/사칙연산 함수/quadratic-operations-function.py
@@ -11,6 +11,8 @@ def process(a,o,c):
         return f'{a} / {c} = {int(a/c)}'
     if o == '*':
         return f'{a} * {c} = {a*c}'
+    if o is not ['+', '-', '/', '*']:
+        return False
 
 result = process(a,o,c)
 print(result)
