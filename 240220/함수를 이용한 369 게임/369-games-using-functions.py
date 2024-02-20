bc56c52141bfc875b7391efd500f332ec4e000f2
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-02-20 17:16:21 +0900

    Title: 함수를 이용한 369 게임, Time: 195ms, Memory: 26MB, Status: Passed - CodeTree

--- 240220/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240220/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,8 +1,11 @@
 a,b = map(int, input().split())
 
 def preprocess(n):
-    if ((n % 10) in [3,6,9]) or ((n//10) in [3,6,9]):
-        return True
+    while n > 0:
+        if (n%10) in [3,6,9]:
+            return True
+        n //= 10
+    return False
 
 def preprocess2(n):
     if preprocess(n) or (n % 3 == 0):
