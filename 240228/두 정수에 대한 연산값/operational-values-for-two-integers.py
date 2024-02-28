6eaefee4f7e61c88c53c1b06e807b9221b63adbd
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-02-28 21:07:36 +0900

    Title: 두 정수에 대한 연산값, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240228/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 240228/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -1,8 +1,16 @@
 a,b = map(int, input().split())
 
 def modify(a,b):
-    n1 = max(a,b) + 25
-    n2 = min(a,b) * 2
-    print(n2, n1)
+    # n1 = max(a,b) + 25
+    # n2 = min(a,b) * 2
+    # print(n2, n1)
+    if a > b:
+        a += 25
+        b *= 2
+    else:
+        b += 25
+        a *=2 
+    print(a,b)
+
 
 modify(a,b)
