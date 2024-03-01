7f32553c0a547ff8697f5b76f04f09928ca99378
bbonghag <103362361+bbonghag@users.noreply.github.com>
2024-03-01 23:42:42 +0900

    Title: 절댓값으로 변경, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240301/절댓값으로 변경/find-the-absolute-value.py
+++ 240301/절댓값으로 변경/find-the-absolute-value.py
@@ -9,3 +9,20 @@ def abs_(n_list):
             print(i, end=' ')
             
 abs_(n_list)
+
+
+# 해설
+# n을 n의 절대값으로 변경
+# def absolute_value(arr):
+#     for i in range(n):
+#         if arr[i] < 0:
+#             arr[i] = -arr[i]
+    
+#     return
+
+
+# absolute_value(arr)
+
+
+# for elem in arr:
+#     print(elem, end=" ")
